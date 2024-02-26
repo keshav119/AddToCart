ec0d4e6cf91e78dbbb99db336320479da162c995
keshav119 <channakeshav99.ck@gmail.com>
2024-02-26 23:26:47 +0530

    setup final amount changes

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ function states(){
     
 function toggleButtonHeight(buttonId) {
 
-
+    setPrice(buttonId);
     moveCircle(buttonId);
+    
 
     for (let [key, value] of map){
         if(key!=buttonId){
@@ -29,17 +30,46 @@ function toggleButtonHeight(buttonId) {
 }
 function moveCircle(buttonId) {
     var circle = document.getElementById(buttonId + 'Circle');
-    var dot = document.getElementById(buttonId + 'Dot'); // Corrected variable name
+    var dot = document.getElementById(buttonId + 'Dot');
+    
 
     // Toggle the 'up' class to control the circle's position
     circle.classList.toggle('up');
     dot.classList.toggle('visible');
+    
+    
 
     if (map.get(buttonId) === 'closed') {
+        
         map.set(buttonId, 'open');
         document.getElementById(buttonId).style.height = '174.11px';
     } else {
+        
         map.set(buttonId, 'closed');
         document.getElementById(buttonId).style.height = '74.11px';
+    }
+}
+
+function setPrice(buttonId){
+    var priceTag = document.getElementById('total');
+    if (map.get(buttonId) === 'closed') {
+        switch (buttonId) {
+            case 'Button1':
+                priceTag.textContent = 'Total : $10.00 USD';
+                break;
+            case 'Button2':
+                // Add logic for Button2 if needed
+                priceTag.textContent = 'Total : $18.00 USD';
+                break;
+            case 'Button3':
+                priceTag.textContent = 'Total : $24.00 USD';
+                break;
+            default:
+                priceTag.textContent = 'Total : $0.00 USD';
+        }
+        
+    } else {
+        priceTag.textContent = 'Total : $0.00 USD';
+        
     }
 }
